7b650f060b59c854aba78fb96fe6f701c464ce9c
Vojtech Nerad <vojtech.nerad2@gmail.com>
2025-04-27 03:32:25 +0200

    About me updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,14 @@ import styled from "styled-components";
 import { Route, Routes } from "react-router";
 import Portfolio from "./pages/Portfolio";
 
-const PageContent = styled.div`
-  max-width: 1280px;
-`;
+const PageContent = styled.div``;
 
 function App() {
   return (
     <>
       <Header />
       <Toolbar />
-      <PageContent>
+      <PageContent className="max-w-7xl mx-auto">
         <Routes>
           <Route path="photography-portfolio">
             <Route path="portfolio" element={<Portfolio />} />
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -2,33 +2,54 @@ import styled from "styled-components";
 import picture from "../assets/vojtech-nerad.jpg";
 import { IoCamera } from "react-icons/io5";
 import { MdLens } from "react-icons/md";
+import { ReactNode } from "react";
 
-const ItemCard = styled.div`
-  background-color: gray;
-`;
+type TItemCard = {
+  icon: ReactNode;
+  brand: string;
+  title: string;
+};
+
+const ItemCard = ({ icon, brand, title }: TItemCard) => {
+  return (
+    <div className="flex items-center bg-blue-500 rounded-md gap-3">
+      <div className="p-3">{icon}</div>
+      <div className="flex flex-col p-3">
+        <span>{brand}</span>
+        <span>{title}</span>
+      </div>
+    </div>
+  );
+};
 
 export default function AboutMe() {
   return (
     <div>
       <h1>O mně</h1>
-      <img src={picture} alt="" width={200} />
-      <p>
-        Fotografii se věnuji už celé dva měsíce — od chvíle, kdy mi škola
-        oznámila, že tohle bude povinný předmět v rámci vedlejší specializace.
-        Od té doby se snažím tvářit, že vím, co dělám, a že clona a čas nejsou
-        jen náhodná slova. Na tomto portfoliu najdete moje pokusy o umění, které
-        vznikly s vypětím sil, trochou štěstí a občas i neplánovanou
-        kreativitou.
-      </p>
+      <div className="flex">
+        <img src={picture} alt="" width={200} />
+        <p>
+          Fotografii se věnuji už celé dva měsíce — od chvíle, kdy mi škola
+          oznámila, že tohle bude povinný předmět v rámci vedlejší specializace.
+          Od té doby se snažím tvářit, že vím, co dělám, a že clona a čas nejsou
+          jen náhodná slova. Na tomto portfoliu najdete moje pokusy o umění,
+          které vznikly s vypětím sil, trochou štěstí a občas i neplánovanou
+          kreativitou.
+        </p>
+      </div>
       <h2>Čím fotím</h2>
-      <ItemCard>
-        <IoCamera size={100} color="white" />
-        <span>Canon EOS 1200D</span>
-      </ItemCard>
-      <ItemCard>
-        <MdLens size={100} color="white" />
-        <span>Canon EF-S 18-55mm f/3.5-5.6 IS II</span>
-      </ItemCard>
+      <div className="flex flex-col gap-2">
+        <ItemCard
+          icon={<IoCamera size={30} />}
+          brand="Canon"
+          title="EOS 1200D"
+        />
+        <ItemCard
+          icon={<MdLens size={30} />}
+          brand="Canon"
+          title=" EF-S 18-55mm f/3.5-5.6 IS II"
+        />
+      </div>
     </div>
   );
 }
