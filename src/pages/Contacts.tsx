b6cf1eeed13dd6ac2c1c198d024c3c51c41e6002
Vojtech Nerad <vojtech.nerad2@gmail.com>
2025-05-26 01:13:11 +0200

    contacts in-progress

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,5 +1,11 @@
 import { ReactNode } from "react";
-import { FaFacebook } from "react-icons/fa6";
+import {
+  FaDove,
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa6";
 import { IoMdMailOpen } from "react-icons/io";
 import styled from "styled-components";
 
@@ -16,10 +22,6 @@ const Card = styled.div`
   border-radius: 7px;
 `;
 
-const StyledRow = styled.tr`
-  font-size: 24px;
-`;
-
 // const Name = styled.div`
 //   font-family: "Corinthia", cursive;
 //   font-weight: 700;
@@ -27,38 +29,119 @@ const StyledRow = styled.tr`
 //   font-size: 40px;
 // `;
 
-type TRow = {
+type LinkButton = {
   icon: ReactNode;
-  title: ReactNode;
+  title: string;
+  hoverColorClass: string;
 };
 
 export default function Contacts() {
-  const Row = ({ icon, title }: TRow) => {
+  const SocialsLink = ({ icon, title, hoverColorClass }: LinkButton) => {
     return (
-      <StyledRow>
-        <td>{icon}</td>
-        <td>{title}</td>
-      </StyledRow>
+      <button
+        className={`flex gap-2 hover:${hoverColorClass} transition hover:cursor-pointer`}
+      >
+        {icon}
+        <span>{title}</span>
+      </button>
     );
   };
 
   return (
-    <Card>
-      {/* <Name>Vojtěch Nerad</Name> */}
-      <table>
-        <Row
-          icon={<IoMdMailOpen size={30} />}
-          title={<td>nevr@vse.cz</td>}
-        ></Row>
-        <Row icon={<IoMdMailOpen />} title={<td>+420 606 123 456</td>}></Row>
-        <Row
-          icon={<FaFacebook />}
-          title={<td>Facebook /vojtech.nerad</td>}
-        ></Row>
-        <Row icon={<span>F</span>} title={<td>Flickr </td>}></Row>
-        <Row icon={<span>IG</span>} title={<td>X </td>}></Row>
-        <Row icon={<span>X</span>} title={<td>@</td>}></Row>
-      </table>
-    </Card>
+    <div className="w-full flex flex-col justify-center">
+      <div className="flex flex-col">
+        <span>
+          Líbí se vám moje práce a chtěli byste se domluvit na focení nebo
+          spolupráci? Napište mi zprávu přes formulář níže - ozvu se co
+          nejdříve.
+        </span>
+        <form className="max-w-md mx-auto p-6 rounded-lg shadow-lg">
+          <label htmlFor="email" className="block mb-2 font-semibold">
+            Kontakt
+          </label>
+          <input
+            type="text"
+            id="text"
+            name="contact"
+            required
+            placeholder="Kontakt na vás"
+            className="w-full mb-4 px-4 py-2 rounded  border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <label htmlFor="message" className="block mb-2 font-semibold">
+            Vzkaz
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            required
+            placeholder="Napište svůj vzkaz..."
+            className="w-full mb-4 px-4 py-2 rounded border border-gray-700 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+          ></textarea>
+        </form>
+        <button className="w-full transition-colors py-3 rounded font-semibold text-white">
+          Odeslat
+        </button>
+
+        {/* Separator */}
+        <div className="flex items-center w-full">
+          <div className="flex-grow h-px bg-gray-300" />
+          <span className="px-3 text-md font-medium">nebo</span>
+          <div className="flex-grow h-px bg-gray-300" />
+        </div>
+
+        <span>Sleduj mě na sociálních sítích</span>
+      </div>
+      <div
+        className="w-full flex flex-col justify-center"
+        style={{ marginTop: 15 }}
+      >
+        <SocialsLink
+          icon={<FaInstagram className="text-xl" />}
+          title="@vojtechnerad"
+          hoverColorClass="text-pink-600"
+        />
+        <SocialsLink
+          icon={<FaLinkedin className="text-xl" />}
+          title="vojtechnerad"
+          hoverColorClass="text-blue-600"
+        />
+        <ul className="space-y-3">
+          <li>
+            <a
+              href="https://www.instagram.com/jannovak"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-3 text-gray-700 hover:text-pink-600 transition"
+            >
+              <FaInstagram className="text-xl" />
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/jannovak"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-3 text-gray-700 hover:text-blue-600 transition"
+            >
+              <FaLinkedin className="text-xl" />
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/jannovak"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-3 text-gray-700 hover:text-black transition"
+            >
+              <FaGithub className="text-xl" />
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
